66b23d85e0489f20277f978868afa4aa714a01cf
Jan Port <48008024+MorgiCZE@users.noreply.github.com>
2021-09-19 10:56:56 +0200

    INITIAL COMMIT

---
+++ beepspace/windows/beepspace/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::beepspace::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::beepspace::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
