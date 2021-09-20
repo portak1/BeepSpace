3e1bbba719614142f5ab597fd52b1fbe8a6c959c
MorgiCZE <48008024+MorgiCZE@users.noreply.github.com>
2021-09-20 21:03:01 +0200

    click update

--- beepspace/App.js
+++ beepspace/App.js
@@ -1,70 +1,54 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View,Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const styles = StyleSheet.create({
-  center: {
-    alignItems: 'center',
-    marginTop: 50
-  }
-})
 
-const Tab = createMaterialBottomTabNavigator();
-
-function HomeTabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      activeColor="#E0B1CB"
-      inactiveColor="#5E548E"
-      barStyle={{ backgroundColor: '#231942' }}>
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Album"
-        component={AlbumScreen}
-        options={{
-          tabBarLabel: 'My albums',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="animation" color={color} size={26} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
-
-function DetailsScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+      <Text>Home Screen</Text>
+      <Button
+      title="go to second screen"
+      onPress={()=> navigation.navigate('Second')}
+      ></Button>
     </View>
   );
 }
 
-const Stack = createStackNavigator();
+
+class cislo {
+  promeny = {pocetKliknuti: 0};
 
 
+
+  
+
+}
+
+function SecondScreen({navigation}){
+  return (   
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Second Screen</Text> 
+      <Button
+      title="go to Home screen"
+      onPress={()=> navigation.navigate('Home')}
+      ></Button>
+      <cislo/>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 const beepspace = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeTabs} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Second" component={SecondScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
